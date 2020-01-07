04e15f93618e9c35c12a739e81aa19715aa69b6a
long wu <alonlong>
2020-01-07 23:15:13 +0800

    update the collector

--- tools/collector/collector.go
+++ tools/collector/collector.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	gateway "TL-Proto/gateway"
+	gateway "TL-Gateway/proto/gateway"
 	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
 	"time"
 
 	"github.com/FusionAuth/go-client/pkg/fusionauth"
@@ -15,7 +16,7 @@ import (
 )
 
 var (
-	address   = flag.String("a", "localhost:15001", "the grpc server address")
+	address   = flag.String("a", "192.168.0.4:15001", "the grpc server address")
 	frequency = flag.Int("f", 500, "the collect frequency(ms)")
 	count     = flag.Int("c", -1, "the count of records")
 )
@@ -40,17 +41,60 @@ func login() string {
 	return response.Token
 }
 
-func makeData(i int) []byte {
-	type Data struct {
-		Name  string
-		Email string
-	}
-	d := Data{
-		Name:  fmt.Sprintf("N%04d", i),
-		Email: "alonlong@163.com",
+// Metric:
+//     {
+//         "source": "fund-falconfund-nyc-mt4-5",
+//         "path": "alpari-brokerage.metatrader.app.cpu.pct",
+//         "data": {value: [0.09]},
+//         "time": 15990932023
+//     }
+
+// CREATE TABLE trace (
+// 	source VARCHAR(100) NOT NULL,
+// 	path VARCHAR(200) NOT NULL,
+// 	type VARCHAR(20) NOT NULL,
+// 	trace VARCHAR(100) NOT NULL,
+// 	hop BOOLEAN NOT NULL,
+// 	host VARCHAR(100) NOT NULL,
+// 	latency INT NOT NULL,
+// 	value FLOAT(2) NOT NULL,
+// 	time TIMESTAMPTZ  NOT NULL,
+// );
+
+type TraceData struct {
+	Type    string  `json:"type"`
+	Trace   string  `json:"trace"`
+	Hop     bool    `json:"hop"`
+	Host    string  `json:"host"`
+	Latency int     `json:"latency"`
+	Value   float32 `json:"value"`
+}
+type NetworkTrace struct {
+	Source string    `json:"source"`
+	Path   string    `json:"path"`
+	Kind   string    `json:"dtype"`
+	Data   TraceData `json:"data"`
+	Time   string    `json:"time"`
+}
+
+func newNetworkTrace(i int) []byte {
+	trace := &NetworkTrace{
+		Kind:   "data_trace",
+		Source: "fund-falconfund-nyc-mt4-5-" + strconv.Itoa(i),
+		Path:   "alpari-brokerage.metatrader.app.trade.network.latency",
+		Data: TraceData{
+			Type:    "ipv4",
+			Trace:   "alpari.london.trade.api -w 2000",
+			Hop:     true,
+			Host:    "alpari.london.trade.api [217.192.86.32]",
+			Latency: 10,
+			Value:   2.99792458e8,
+		},
+		// Time: "2015-01-12 12:45:00.345+08",
+		Time: "2015-01-12 12:45:01",
 	}
 
-	b, _ := json.Marshal(&d)
+	b, _ := json.Marshal(&trace)
 	return b
 }
 
@@ -75,7 +119,7 @@ func main() {
 
 		request := gateway.ReportRequest{
 			Token: token,
-			Data:  makeData(i),
+			Data:  newNetworkTrace(i),
 		}
 		if _, err := c.Report(context.Background(), &request); err != nil {
 			fmt.Printf("report data: %v\n", err)
