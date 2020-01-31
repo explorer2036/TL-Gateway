5449e834efd8c3a471c8a2d3eb81cb92c2f41681
alonlong <alonlong@163.com>
2020-01-31 19:53:20 +0800

    update the collector tool

--- tools/collector/collector.go
+++ tools/collector/collector.go
@@ -8,10 +8,9 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"os"
-	"os/signal"
 	"strconv"
-	"syscall"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/FusionAuth/go-client/pkg/fusionauth"
@@ -25,18 +24,20 @@ var (
 	connections = flag.Int("s", 1, "the count of connections")
 	dataType    = flag.String("t", "data_trace", "the data type(data_trace/data_quote)")
 	action      = flag.String("n", "insert", "the action of message(insert/update)")
+	loginid     = flag.String("u", "aqliodlong@126.com", "the username for fusion")
+	password    = flag.String("p", "XjsAZxOajA", "the password for fusion")
 )
 
 // login the fusion
-func login() string {
+func login(loginid string, password string) (string, string) {
 	apiKey := "Kc-ntFUr767Qyk0RgbVU8dxLLdjmTM98_XjsAZxOajA"
 	baseURL, _ := url.Parse("http://209.222.106.245:9011/")
 	httpClient := &http.Client{Timeout: time.Second * 5}
 	auth := fusionauth.NewClient(httpClient, baseURL, apiKey)
 
 	var credentials fusionauth.LoginRequest
-	credentials.LoginId = "alon@traderlinked.com"
-	credentials.Password = "XjsAZxOajA"
+	credentials.LoginId = loginid
+	credentials.Password = password
 
 	// Use FusionAuth Go client to log in the user
 	response, _, err := auth.Login(credentials)
@@ -44,28 +45,8 @@ func login() string {
 		panic(err)
 	}
 
-	return response.Token
+	return response.Token, response.User.Id
 }
-
-// Metric:
-//     {
-//         "source": "fund-falconfund",
-//         "path": "cpu.pct",
-//         "data": {value: [0.09]},
-//         "time": 15990932023
-//     }
-
-// CREATE TABLE trace (
-// 	source VARCHAR(100) NOT NULL,
-// 	path VARCHAR(200) NOT NULL,
-// 	type VARCHAR(20) NOT NULL,
-// 	trace VARCHAR(100) NOT NULL,
-// 	hop BOOLEAN NOT NULL,
-// 	host VARCHAR(100) NOT NULL,
-// 	latency INT NOT NULL,
-// 	value FLOAT(2) NOT NULL,
-// 	time TIMESTAMPTZ  NOT NULL,
-// );
 
 type QuoteData struct {
 	Ticker string  `json:"ticker"`
@@ -76,7 +57,7 @@ type QuoteData struct {
 }
 
 type NetworkQuote struct {
-	UserID string    `json:"userid"`
+	UserID int       `json:"userid"`
 	Source string    `json:"source"`
 	Path   string    `json:"path"`
 	Kind   string    `json:"dtype"`
@@ -102,11 +83,11 @@ type NetworkTrace struct {
 	Time   string    `json:"time"`
 }
 
-func newNetworkQuote(i int, kind string, action string) []byte {
+func newNetworkQuote(i int, userid int, kind string, action string) []byte {
 	quote := &NetworkQuote{
 		Kind:   kind,
 		Action: action,
-		UserID: "test@live.cn",
+		UserID: userid,
 		Source: "fund-falconfund-" + strconv.Itoa(i),
 		Path:   "trade.network.latency",
 		Data: QuoteData{
@@ -114,7 +95,7 @@ func newNetworkQuote(i int, kind string, action string) []byte {
 			Last:   1.9717,
 			Bid:    1.9718,
 			Ask:    1.9719,
-			Time:   time.Now().Format("2006-02-01 15:04:05 +0800 CST"),
+			Time:   time.Now().Format("2006-01-02 15:04:05"),
 		},
 	}
 
@@ -136,14 +117,16 @@ func newNetworkTrace(i int, kind string, action string) []byte {
 			Latency: 10,
 			Value:   2.997,
 		},
-		Time: time.Now().Format("2006-02-01 15:04:05 +0800 CST"),
+		Time: time.Now().Format("2006-01-02 15:04:05"),
 	}
 
 	b, _ := json.Marshal(&trace)
 	return b
 }
 
-func do(token string, dtype string, action string) {
+func do(wg *sync.WaitGroup, token string, userid int, dtype string, action string) {
+	defer wg.Done()
+
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(*address, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(5*time.Second))
 	if err != nil {
@@ -159,10 +142,10 @@ func do(token string, dtype string, action string) {
 
 		var data []byte
 		if dtype == "data_trace" {
-			data = newNetworkTrace(i, dtype, action)
+			data = newNetworkTrace(0, dtype, action)
 		}
 		if dtype == "data_quote" {
-			data = newNetworkQuote(i, dtype, action)
+			data = newNetworkQuote(0, userid, dtype, action)
 		}
 
 		request := gateway.ReportRequest{
@@ -186,6 +169,19 @@ func do(token string, dtype string, action string) {
 	}
 }
 
+// dbd62208-d0ea-4a5a-9066-64d41e7dcedd
+// 8+4+4+4+12
+// for the uuid format, i will put the user id with 10 bytes replace the part "d0ea-4a5a-90"
+func uuid2id(uuid string) int {
+	if len(uuid) != 36 {
+		panic(uuid)
+	}
+
+	out := strings.Replace(uuid[9:21], "-", "", -1)
+	id, _ := strconv.Atoi(out)
+	return id
+}
+
 func main() {
 	flag.Parse()
 
@@ -194,19 +190,15 @@ func main() {
 	}
 
 	// login the fusion to fetch the token
-	token := login()
+	token, uuid := login(*loginid, *password)
 
+	userid := uuid2id(uuid)
+
+	var wg sync.WaitGroup
 	for index := 0; index < *connections; index++ {
-		go do(token, *dataType, *action)
+		wg.Add(1)
+		go do(&wg, token, userid, *dataType, *action)
 	}
 
-	sig := make(chan os.Signal, 1024)
-	// subscribe signals: SIGINT & SINGTERM
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	for {
-		select {
-		case <-sig:
-			return
-		}
-	}
+	wg.Wait()
 }
